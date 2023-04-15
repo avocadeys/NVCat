f4f963734d6eb657ea7d00756917d5cb71815692
avocadeys <glenjauodpersonal@gmail.com>
2023-04-15 17:07:19 +0800

    Tweak nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,7 +8,25 @@ return {
     opts = {
       renderer = {
         root_folder_label = false,
-      }
+        add_trailing = false,
+        group_empty = true,
+        highlight_git = false,
+        full_name = false,
+        highlight_opened_files = "none",
+        special_files = {
+          "Cargo.toml", "Makefile", "README.md", "readme.md", "CMakeLists.txt"
+        },
+        symlink_destination = true,
+        indent_markers = {
+          enable = true,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            item = "│ ",
+            none = "  ",
+          },
+        },
+      },
     }
   }
 }
