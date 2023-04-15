2901049994ec43d629b46dde768bf792c17fe39d
avocadeys <glenjauodpersonal@gmail.com>
2023-04-15 20:43:04 +0800

    Tweak todo-comments

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,9 +1,12 @@
 return {
   {
     "folke/todo-comments.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
     cmd = { "TodoTrouble", "TodoTelescope" },
     event = { "BufReadPost", "BufNewFile" },
-    config = true,
+    config = function()
+      require("todo-comments").setup{}
+      end,
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
