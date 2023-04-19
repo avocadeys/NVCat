a4c303c2b6170acfd746c258196daca64e1b56e7
avocadeys <glenjauodpersonal@gmail.com>
2023-04-19 22:53:45 +0800

    Setup flutter-tools

--- lua/plugins/flutter-tools.lua
+++ lua/plugins/flutter-tools.lua
@@ -8,7 +8,40 @@ return {
     },
 
     config = function()
-    require("flutter-tools").setup {}
+    require("flutter-tools").setup ({
+      debugger = {
+        enabled = true,
+        run_via_dap = true,
+      },
+      outline = {
+        auto_open = false
+      },
+      decorations = {
+        statusline = {
+          device = true, app_version = true
+        },
+      },
+      widget_guides = {
+        enabled = true, debug = true
+      },
+      dev_log = {
+        enabled = true, open_cmd = "tabedit"
+      },
+      flutter_path = "/home/zubarev/fvm/default/bin",
+      fvm = true,
+      lsp = {
+        color = {
+          enabled = true,
+          background = true,
+          virtual_text = false,
+        },
+        settings = {
+          showTodos = true,
+          renameFilesWithClasses = "prompt",
+        },
+        capabilities = require("cmp_nvim_lsp").default_capabilities,
+      },
+    })
     end
   }
 }
