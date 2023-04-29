adbdbcded19c3a66d67ad75e100f0720d18ffa5f
avocadeys <glenjauodpersonal@gmail.com>
2023-04-30 00:03:58 +0800

    Change some icons for git & remove unnecessary codes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,31 +1,43 @@
 return {
   {
     "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    lazy = true,
+    cmd = {
+      "NvimTreeToggle",
+      "NvimTreeOpen",
+      "NvimTreeFindFile",
+      "NvimTreeFindFileToggle",
+      "NvimTreeRefresh",
     },
+    dependencies = "nvim-tree/nvim-web-devicons",
     opts = {
       renderer = {
         root_folder_label = false,
-        add_trailing = false,
-        group_empty = true,
-        highlight_git = false,
-        full_name = false,
-        highlight_opened_files = "none",
-        special_files = {
-          "Cargo.toml", "Makefile", "README.md", "readme.md", "CMakeLists.txt"
-        },
-        symlink_destination = true,
         indent_markers = {
           enable = true,
+          inline_arrows = true,
           icons = {
-            corner = "└ ",
-            edge = "│ ",
-            item = "│ ",
-            none = "  ",
+            corner = "└",
+            edge = "│",
+            item = "│",
+            bottom = "─",
+            none = " ",
           },
         },
+        icons = {
+          git_placement = "after",
+          glyphs = {
+            git = {
+              unstaged = "U",
+              staged = "S",
+              unmerged = "",
+              renamed = "R",
+              untracked = "?",
+              deleted = "D",
+              ignored = "◌",
+            },
+          },
+        }
       },
     }
   }
